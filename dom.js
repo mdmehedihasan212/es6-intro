6d54b64ffa25e271d7732cab72d04c09be6a872d
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-20 19:27:48 +0600

    dom-start

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.getElementById('add-border').addEventListener('click', function () {
+    const containerFriends = document.getElementById('friend-container');
+    containerFriends.style.border = '3px solid red';
+})
