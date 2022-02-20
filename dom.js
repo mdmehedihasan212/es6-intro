3f09e8cb25900a31019b14a3f5b7c6f39d335fb5
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-20 21:15:48 +0600

    add appendchild

--- dom.js
+++ dom.js
@@ -1,4 +1,22 @@
 document.getElementById('add-border').addEventListener('click', function () {
     const containerFriends = document.getElementById('friend-container');
     containerFriends.style.border = '3px solid red';
-})
+});
+
+function addBackgroundColor() {
+    const friends = document.getElementsByClassName('friend');
+    for (const friend of friends) {
+        friend.style.backgroundColor = 'red';
+    };
+}
+
+function addFriend() {
+    const addMoreFnd = document.getElementById('friend-container');
+    const friendAdd = document.createElement('div');
+    friendAdd.classList.add('friend');
+    friendAdd.innerHTML = `
+    <h3 class="friend-name">My friends Bahadur</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+    `;
+    addMoreFnd.appendChild(friendAdd);
+}
